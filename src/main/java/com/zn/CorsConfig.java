b37a961ef4d212b99146f8850aed1239d5649bf2
ronterambabu <rambabu.r@zynlogic.com>
2025-06-23 18:05:32 +0530

    fix: Update CORS configuration to include localhost and Vercel origins

--- src/main/java/com/zn/CorsConfig.java
+++ src/main/java/com/zn/CorsConfig.java
@@ -11,8 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all endpoints
                 .allowedOriginPatterns(
-                    "https://renewable-meet-2026.vercel.app"
-                    
+                    "https://renewable-meet-2026.vercel.app",
+                    "http://localhost:*"
                 ) // Allow only Vercel and local dev origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
