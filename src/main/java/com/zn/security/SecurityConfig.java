3023015a7b7e4004486369de8b5d169eae089a43
ronterambabu <rambabu.r@zynlogic.com>
2025-06-24 13:17:49 +0530

    f3

--- src/main/java/com/zn/security/SecurityConfig.java
+++ src/main/java/com/zn/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("https://renewable-meet-2026.vercel.app","http://localhost:*", "https://localhost:*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("https://renewable-be.onrender.com","https://renewable-meet-2026.vercel.app","http://localhost:*", "https://localhost:*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true); // Important for cookies
